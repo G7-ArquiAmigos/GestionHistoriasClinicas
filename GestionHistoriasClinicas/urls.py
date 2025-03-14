5e85055fc1d45e1d6f3df4bd102da4abb9c86468
Lucasgali03 <123596953+Lucasgali03@users.noreply.github.com>
2025-03-14 07:45:33 -0500

    Update urls.py

--- GestionHistoriasClinicas/urls.py
+++ GestionHistoriasClinicas/urls.py
@@ -19,7 +19,7 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', csrf_exempt(admin.site.urls)),
     path('home/',views.home),
     path('historiaClinica/',include('HistoriaClinica.urls')),
     path('', views.index),
