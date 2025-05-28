66467b209cd299bcaf87e5897d4ddd413e126581
Tdiazv2 <123430733+Tdiazv2@users.noreply.github.com>
2025-05-28 18:43:52 -0500

    Update urls.py

--- HistoriaClinica/urls.py
+++ HistoriaClinica/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 
-urlpatterns = [path('',views.historias_clinicas_view,name='historias_clinicas_view'),
+urlpatterns = [path('historiaClinica/',views.historias_clinicas_view,name='historias_clinicas_view'),
                path('historiacreate/', csrf_exempt(views.historias_create), name='historiacreate'),]
